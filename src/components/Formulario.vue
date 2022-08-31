a2921c65f50efa40c41bcd13656f67593031c73d
Levy <levyronald3@gmail.com>
2022-08-31 11:38:20 -0300

    chore(arquivos iniciais): CursoVue03(01) Aula01 (completo)

---
+++ src/components/Formulario.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulario para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="qual tarefa você deseja iniciar?"
+        />
+      </div>
+      <div class="column">
+        <div
+          class="is-flex is-align-items-center is-justify-content-space-between"
+        >
+          <section>
+            <strong>{{ tempoDecorrido }}</strong>
+          </section>
+          <button class="button" @click="iniciar">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button" @click="finalizar">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FormularioApp",
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0
+    };
+  },
+  computed: {
+    tempoDecorrido() : string {
+        return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
+    }
+  },
+  methods: {
+    iniciar() {
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1
+      }, 1000);
+    },
+    finalizar() {
+        clearInterval(this.cronometro)
+    },
+  },
+});
+</script>
+<style scoped>
+</style>
